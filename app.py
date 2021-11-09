ece039767cc7a74d33c168dc862a985e5440a80e
Eliyahu Masinter <eliyahumasinter@gmail.com>
2021-11-10 01:17:25 +0200

    minor fix to makeBold

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from paths import *
 from functions import *
 
-app.jinja_env.globals.update(makeBold=makeBold)
+app.jinja_env.globals.update(markUp=markUp)
 app.config.from_object(Config)
 
 if __name__ == '__main__':
