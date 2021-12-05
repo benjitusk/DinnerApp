744b8f94541f49676b23d03da5d0fe45456052d8
Eliyahu Masinter <eliyahumasinter@gmail.com>
2021-12-05 18:29:06 +0200

    Added bootstrap to setup.py and

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from jinja2 import select_autoescape, Environment, FileSystemLoader
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 import matplotlib.pyplot as plt
 import pandas as pd
 import logging
@@ -11,6 +12,7 @@ import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s@%(funcName)s:%(lineno)d %(levelname)s:\t%(message)s')
 logger = logging
